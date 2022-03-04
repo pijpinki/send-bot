393322158a35ab0d8070881c89164ffa3bb81f4a
pavel <pijpinki>
2022-03-04 11:53:07 +0200

    Added tcp test server

---
+++ src/test-utils/tcp-server.js
@@ -0,0 +1,26 @@
+import Net from 'net'
+
+const port = 8080;
+const server = new Net.Server();
+
+server.listen(port, function() {
+  console.log(`Server listening for connection requests on socket localhost:${port}`);
+});
+
+server.on('connection', function(socket) {
+  console.log('A new connection has been established.');
+
+  socket.write('Hello, client.');
+
+  socket.on('data', function(chunk) {
+    console.info('data received');
+  });
+
+  socket.on('end', function() {
+    console.log('Closing connection with the client');
+  });
+
+  socket.on('error', function(err) {
+    console.log(`Error: ${err}`);
+  });
+});
